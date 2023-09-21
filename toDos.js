56795473a065bee22419879d21e0c6ae2d6b71b5
kiana esmaili <kianaesmaili18@gmail.com>
2023-09-21 09:25:58 +0330

    toDos completed

--- toDos.js
+++ toDos.js
@@ -4,57 +4,120 @@ home.addEventListener("click", () => {
   location.assign("home.html");
 });
 
+document.addEventListener("DOMContentLoaded", () => {});
+// --------------------------- create todos item ---------------------------
+function createTodos(currentData) {
+  itemContainer.innerHTML = "";
+
+  if (currentData.length < 1) {
+    location.assign("error.html");
+    return;
+  }
+  for (let item of currentData) {
+    let dueTime = new Date(item.dueDate).toLocaleDateString("en-US");
+    if (item.checked) {
+      itemContainer.innerHTML += `
+  <div class="items">
+      <div class="items-top">
+        <span>
+        <input type="checkbox" name="check" id="check" checked onclick="sendToServerCheck(${item.id})" />
+          <span class="title">${item.title}</span>
+          <span class="time">${dueTime}</span>
+        </span>
+        <span class="icones">
+          <i class="fas fa-pen one" onclick="editTaskBtnClicked(${item.id})"></i>
+          <i class="fas fa-trash two" onclick="deleteTaskBtnClicked(${item.id})"></i>
+        </span>
+      </div>
+      <p class="paragraph">${item.description}</p>
+    </div>`;
+    } else {
+      itemContainer.innerHTML += `
+  <div class="items">
+      <div class="items-top">
+        <span>
+        <input type="checkbox" name="check" id="check" onclick="sendToServerCheck(${item.id})" />
+          <span class="title">${item.title}</span>
+          <span class="time">${dueTime}</span>
+        </span>
+        <span class="icones">
+          <i class="fas fa-pen one" onclick="editTaskBtnClicked(${item.id})"></i>
+          <i class="fas fa-trash two" onclick="deleteTaskBtnClicked(${item.id})"></i>
+        </span>
+      </div>
+      <p class="paragraph">${item.description}</p>
+    </div>`;
+    }
+  }
+}
+// --------------------------- create todos item ---------------------------
+
 // --------------------------- get data from server and show ---------------------------
+const itemContainer = document.querySelector("main");
+const paginatedList = document.querySelector(".paginatedList");
+
 let data = [];
 async function getApi() {
+  // first page
+  if (location.href === "http://127.0.0.1:5501/toDos.html") {
+    let url = new URL(location.href);
+    url.searchParams.set("page", 1);
+    location.assign(url);
+  }
+  // first page
+
   try {
     let serverData = await fetch(
       "https://6506fb123a38daf4803ef566.mockapi.io/Todos/todoes-text"
     );
     data = await serverData.json();
+
+    // create first page task
+    // createTodos(data.slice(0, 10));
+    // create first page task
+
     console.log(data);
   } catch (error) {
     console.log(`error :::::: ${error}`);
   }
-  // --------------------------- create todos item ---------------------------
-  const itemContainer = document.querySelector("main");
-  const paginatedList = document.querySelector(".paginatedList");
-  function createTodos() {
-    itemContainer.innerHTML = "";
-    // pagination
-    // let pages = Math.ceil(data.length / 10);
-    // console.log(pages);
-    // for (let i = 1; i <= pages.length; i++) {
-    //   let li = document.createElement("li");
-    //   li.innerHTML = i;
-    //   paginatedList.append(li);
-    //   li.addEventListener("click", showCurrentPageToDo);
-    // }
-    // function showCurrentPageToDo() {}
-    // pagination
-    for (let item of data) {
-      let dueTime = new Date(item.dueDate).toLocaleDateString("en-US");
-
-      itemContainer.innerHTML += `
-      <div class="items">
-          <div class="items-top">
-            <span>
-              <span class="title">${item.title}</span>
-              <span class="time">${dueTime}</span>
-            </span>
-            <span class="icones">
-              <i class="fas fa-pen one" onclick="editTaskBtnClicked(${item.id})"></i>
-              <i class="fas fa-trash two" onclick="deleteTaskBtnClicked(${item.id})"></i>
-            </span>
-          </div>
-          <p class="paragraph">${item.description}</p>
-        </div>`;
-    }
-  }
-  createTodos();
-  // --------------------------- create todos item ---------------------------
+  pagination();
 }
 getApi();
+
+// pagination
+function pagination() {
+  if (location.href !== "http://127.0.0.1:5501/toDos.html") {
+    let url = new URL(location.href);
+    let pageNumber = url.searchParams.get("page");
+    createTodos(data.slice((pageNumber - 1) * 10, pageNumber * 10));
+    // return;
+  }
+  let pageCount = Math.ceil(data.length / 10);
+  // console.log(data.length, pageCount);
+  for (let i = 1; i <= pageCount; i++) {
+    // console.log(i);
+    let li = document.createElement("li");
+    li.innerHTML = i;
+    paginatedList.append(li);
+    li.addEventListener("click", () => {
+      try {
+        let url = new URL(location.href);
+        url.searchParams.set("page", i);
+        location.assign(url.href);
+        // window.location.search = url;
+
+        let toDosEachPageArray = data.slice((i - 1) * 10, i * 10);
+        console.log(i, toDosEachPageArray);
+        createTodos(toDosEachPageArray);
+      } catch (error) {
+        console.log(error);
+        location.assign("error.html");
+      }
+    });
+  }
+}
+// pagination
+
 // --------------------------- get data from server and show ---------------------------
 
 // --------------------------- delete task ---------------------------
@@ -75,11 +138,12 @@ function deleteTaskBtnClicked(taskId) {
       modalDate.innerHTML = new Date(item.dueDate).toLocaleDateString("en-US");
     }
   }
-  console.log(modalTitle.id, modalTitle.innerHTML);
+  // console.log(modalTitle.id, modalTitle.innerHTML);
 }
 // on click
 
-function finalDelete() {
+function finalDelete(e) {
+  e.preventDefault();
   fetch(
     `https://6506fb123a38daf4803ef566.mockapi.io/Todos/todoes-text/${currentId}`,
     {
@@ -88,15 +152,13 @@ function finalDelete() {
   )
     .then((res) => {
       if (res.status === 200) {
-        // console.log(res.json(), `task deleted`);
         console.log("deleted");
-        // location.reload();
         getApi();
-      } else {
-        alert(res.status);
-      }
+      } else if (res.status === 404) {
+        location.assign("error.html");
+      } else alert(res.statusText);
     })
-    .catch((error) => console.log(error))
+    .catch((error) => location.assign("error.html"))
     .finally((modal.style.display = "none"));
   // delete from server
 }
@@ -106,12 +168,68 @@ cancelTaskBtn.addEventListener("click", () => {
 });
 
 // --------------------------- delete task ---------------------------
-const inputTitle = document.querySelector(".title");
-const description = document.querySelector("#description");
-const inputDate = document.querySelector("#date");
+
 // on click
-function editTaskBtnClicked(id) {
-  location.assign("home.html/id=1");
+const edit = document.querySelector(".one");
+
+function editTaskBtnClicked(taskId) {
+  console.log("edit click");
+  let taskUrl = new URL("http://127.0.0.1:5501/home.html");
+  taskUrl.searchParams.append("id", taskId);
+  location.assign(taskUrl);
+}
+// on click
+
+// --------------------------- edit task ---------------------------
+
+// --------------------------- checkbox---------------------------
+
+// on click
+const checkBtn = document.querySelectorAll("#check");
+
+async function sendToServerCheck(id) {
+  // for (let item in data) {
+  //   if (item.id == +id) {
+
+  //   } else console.log("not found", id, item);
+  // }
+  if (!this.checked) {
+    this.checked = true;
+    let putFetch = await fetch(
+      `https://6506fb123a38daf4803ef566.mockapi.io/Todos/todoes-text/${id}`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          checked: true,
+        }),
+      }
+    )
+      .then((res) => {
+        if (res.status === 200) {
+          console.log("checked in server");
+        } else console.log(res.statusText);
+      })
+      .catch((error) => console.log(error));
+  } else if (this.checked) {
+    this.checked = false;
+    let putFetch = await fetch(
+      `https://6506fb123a38daf4803ef566.mockapi.io/Todos/todoes-text/${id}`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          checked: false,
+        }),
+      }
+    )
+      .then((res) => {
+        if (res.status === 200) {
+          console.log("unchecked in server");
+        } else console.log(res.statusText);
+      })
+      .catch((error) => console.log(error));
+  }
 }
 // on click
 
