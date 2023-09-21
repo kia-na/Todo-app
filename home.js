127548c42960c1f8e268e050e17dd998d66f04ef
kiana esmaili <kianaesmaili18@gmail.com>
2023-09-21 09:23:55 +0330

    home completed

--- home.js
+++ home.js
@@ -16,7 +16,7 @@ todos.addEventListener("click", () => {
 function areInputsValid() {
   if (inputTitle.value === "") {
     titleError.style.display = "inline";
-    console.log(inputTitle.value);
+    // console.log(inputTitle.value);
     return false;
   } else if (inputTitle.value !== "") {
     titleError.style.display = "none";
@@ -39,20 +39,26 @@ function areInputsValid() {
 
   function useRegex(input) {
     let regex = /\d\d\/\d\d\/\d\d\d\d/i;
-    let regex2 = /\d\d\d\d\/\d\d\/\d\d/i;
-    return regex.test(input) || regex2.test(input);
+    let regex2 = /\d\/\d\/\d\d\d\d/i;
+    let regex3 = /\d\d\d\d\/\d\d\/\d\d/i;
+    let regex4 = /\d\d\d\d\/\d\/\d/i;
+    return (
+      regex.test(input) ||
+      regex2.test(input) ||
+      regex3.test(input) ||
+      regex4.test(input)
+    );
   }
 }
 // ----------------------- inputs validation -----------------------
 
 // ----------------------- send new todo to server -----------------------
-let id = 16;
+let id = 43;
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   if (!areInputsValid()) {
     return;
   }
-
   let item = {};
   item.title = inputTitle.value;
   item.description = description.value;
@@ -71,7 +77,7 @@ submitBtn.addEventListener("click", (e) => {
     .then((res) => {
       if (res.ok) {
         console.log(res.json());
-        showToast();
+        showToast("Task Saved");
         inputTitle.value = "";
         description.value = "";
         inputDate.value = "";
@@ -84,15 +90,100 @@ submitBtn.addEventListener("click", (e) => {
 // ----------------------- send new todo to server -----------------------
 
 // ----------------------- toast -----------------------
-function showToast(text = "Task Saved") {
-  let toat = document.getElementById("toast");
-  toat.className = "show";
+function showToast(text) {
+  let toast = document.getElementById("toast");
+  toast.className = "show";
   setTimeout(function () {
-    toat.className = toat.className.replace("show", "");
+    toast.className = toast.className.replace("show", "");
   }, 3000);
 }
+
 // ----------------------- toast -----------------------
 
+// ----------------------- handle edit task -----------------------
+
+document.addEventListener("DOMContentLoaded", isEdit);
+const saveBtn = document.querySelector("#save");
+
+let editId;
+function isEdit() {
+  console.log(location.href);
+  if (location.href !== "http://127.0.0.1:5501/home.html") {
+    let Url = new URLSearchParams(document.location.search);
+    editId = Url.get("id");
+    console.log(editId);
+    editTask(editId);
+    return true;
+  }
+}
+async function editTask(id) {
+  let taskInfo = await fetch(
+    `https://6506fb123a38daf4803ef566.mockapi.io/Todos/todoes-text/${id}`,
+    {
+      method: "GET",
+    }
+  ).catch((error) => console.log(error));
+
+  let taskJson = await taskInfo.json();
+  // fill inputs with data
+  inputTitle.value = taskJson.title;
+  description.value = taskJson.description;
+  inputDate.value = new Date(taskJson.dueDate).toLocaleDateString("en-US");
+  // fill inputs with data
+
+  // change button
+  saveBtn.style.display = "inline";
+  submitBtn.style.display = "none";
+  // change button
+
+  // click on save
+  saveBtn.addEventListener("click", saveBtnClicked);
+  // click on save
+
+  async function saveBtnClicked(e) {
+    e.preventDefault();
+    if (!areInputsValid()) {
+      return;
+    }
+    let d = new Date();
+    let updatedTime = d.getTime();
+
+    let putFetch = await fetch(
+      `https://6506fb123a38daf4803ef566.mockapi.io/Todos/todoes-text/${id}`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title: inputTitle.value,
+          description: description.value,
+          dueDate: inputDate.value,
+          updateAt: updatedTime,
+        }),
+      }
+    )
+      .catch((error) => console.log(error))
+      .then((res) => {
+        console.log(res);
+        showToast("Task Edited");
+        inputTitle.value = "";
+        description.value = "";
+        inputDate.value = "";
+
+        setTimeout(() => {
+          location.assign("http://127.0.0.1:5501/home.html");
+          // const homeUrl = new URL(location.href);
+          // const params = new URLSearchParams(homeUrl.search);
+          // params.delete("id");
+          // location.assign(params);
+        }, 3500);
+      });
+  }
+
+  console.log(taskJson);
+  console.log(editId);
+}
+
+// ----------------------- handle edit task -----------------------
 // {
 //   checked: false;
 //   createAt: "2023-04-27T13:58:41.929Z";
